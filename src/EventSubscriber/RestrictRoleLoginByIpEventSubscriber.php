7a93f50d92bc6df3c45697ee55da4f8070bb6d38
karan <karan@644760.no-reply.drupal.org>
2021-09-30 14:21:38 +0530

    Issue #3238163 by karan_mudi: D9 compability fixes (Deprecated Code Report)

--- src/EventSubscriber/RestrictRoleLoginByIpEventSubscriber.php
+++ src/EventSubscriber/RestrictRoleLoginByIpEventSubscriber.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\restrict_role_login_by_ip\EventSubscriber;
 
+use Drupal\Core\Messenger\MessengerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -11,12 +14,33 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class RestrictRoleLoginByIpEventSubscriber implements EventSubscriberInterface {
 
   /**
-   * Set message when user is denied access.
+   * Messenger service.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
    */
-  public function onKernelRequest($event) {
-    $queryLogout = \Drupal::request()->query->get('logout');
+  protected $messenger;
+
+  /**
+   * Constructs the event subscriber object.
+   *
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   Messenger service.
+   */
+  public function __construct(MessengerInterface $messenger) {
+    $this->messenger = $messenger;
+  }
+
+  /**
+   * Set message when user is denied access.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
+   *   The kernel request event.
+   */
+  public function onKernelRequest(RequestEvent $event) {
+    $request = $event->getRequest();
+    $queryLogout = $request->query->get('logout');
     if (!empty($queryLogout) && $queryLogout == 'restrict') {
-      drupal_set_message(t('You are not allowed to login from this IP address. Please contact the Site Administrator.'), 'error', TRUE);
+      $this->messenger->addMessage(t('You are not allowed to login from this IP address. Please contact the Site Administrator.'), 'error', TRUE);
     }
   }
 
