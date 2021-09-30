bde5b99b90ac2e08b148df446a071e6d9cf54502
Supreetam09 <Supreetam09@3673878.no-reply.drupal.org>
2021-09-30 21:57:24 +0530

    Issue #3240046 by Supreetam09, mohit_aghera: Access restricted message shown even after restriction is removed

--- src/EventSubscriber/RestrictRoleLoginByIpEventSubscriber.php
+++ src/EventSubscriber/RestrictRoleLoginByIpEventSubscriber.php
@@ -4,9 +4,11 @@ namespace Drupal\restrict_role_login_by_ip\EventSubscriber;
 
 use Drupal\Core\Messenger\MessengerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\Core\Url;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
  * Event Subscriber RestrictRoleLoginByIpEventSubscriber.
@@ -33,14 +35,15 @@ class RestrictRoleLoginByIpEventSubscriber implements EventSubscriberInterface {
   /**
    * Set message when user is denied access.
    *
-   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
+   * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
    *   The kernel request event.
    */
-  public function onKernelRequest(RequestEvent $event) {
+  public function onKernelRequest(GetResponseEvent $event) {
     $request = $event->getRequest();
     $queryLogout = $request->query->get('logout');
     if (!empty($queryLogout) && $queryLogout == 'restrict') {
       $this->messenger->addMessage(t('You are not allowed to login from this IP address. Please contact the Site Administrator.'), 'error', TRUE);
+      $event->setResponse(new RedirectResponse(Url::fromRoute('user.login')->toString()));
     }
   }
 
